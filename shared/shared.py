d61c354ee8bd4f14302befc5a61c53a6c1387575
JD-DS <101203302+JD-DS@users.noreply.github.com>
2023-02-14 22:33:05 -0500

    Add files via upload

---
+++ shared/shared.py
@@ -0,0 +1,30 @@
+import re
+
+
+def afunction():
+    print("This is the installed function")
+
+
+def clean_string(str_word):
+    """
+    INPUTS:
+    str_word     string string contining several words to clean
+    RETURNS:
+    string       the string after removal of extra spaces, punctuation and lowercasing
+    """
+    str_word = re.sub(r'\W+', ' ', str_word)
+    assert isinstance(
+        str_word, str), "String expected but recieved type {} instead".format(type(str_word))
+
+    return str_word.strip()
+
+
+def space_compress(stocomp):
+    assert isinstance(
+        stocomp, str), "Expected str but got {} instead".format(type(stocomp))
+    comp = re.sub(r'\s+', ' ', stocomp)
+    return comp.strip()
+
+
+def new_func():
+    pass
